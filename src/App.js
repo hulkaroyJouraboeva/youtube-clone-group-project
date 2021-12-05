1a1771cfb223ca803cb4749b782df5ba0a882cae
Triane Peart <TrianePeart@gmail.com>
2021-12-05 13:39:07 -0500

    Many Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from "react"
+import React from "react";
+import "./App.css";
+import {Route, Routes} from "react-router-dom";
+import Nav from ".Components/Nav";
+import Result from ".Components/Result"
+import About from ".Components/About"
+import Search from ".Components/Search"
 
-fetch(
-  `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
-);
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state ={}
+  }
+  render(){
+    return (
+      <div className="App">
+        <Nav/>
+        <main>
+          <Routes
+            <Route exact path="/" element={
+              //I FEEL AS IF THIS IS CODED CORRECTLY BUT THERE ARE RED LINES. PLEASE LOOK OVER FOR SYNTAX ERROR. 
+              <>
+              <Search/>
+              <Result/>
+              </>
+            }
+          />
+           <Routes
+            <Route exact path= "/video/:id" 
+            //SAME ISSUE AS ABOVE
+            element={
+              <>
+              <Search/>
+              <Video/>
+              </>
+            }
+          />
+          <Route path="/about/" element ={<About/>}/>
+        </main>
+      </div>
+    )
+  }
+}
 
 
 export default App;
