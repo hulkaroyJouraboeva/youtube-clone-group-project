2aadeba6a865a3c3a6b81dd2e6aae17c9e15d203
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-12-08 11:33:59 -0500

    created Home, SearchBar, Thumbnail components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Home from "./components/Home";
 
-
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    )
+  }
+}
 
 export default App;
