2e8696c5026df3e80c8d92110fda2b1b71741f5f
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-12-09 10:22:59 -0500

    worked on fetch, updated api link

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,43 @@
 import React from 'react';
+import Thumbnail from './Thumbnail';
 
 class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
+    // state to hold userInput and submitted
+    constructor() {
+        super();
         this.state = {
-            userInput: ''
+            userInput: '',
+            submitted: false,
         }
     }
 
+    // method to handle submit
     handleSubmit = (event) => {
+        event.preventDefault()
         this.setState({
-            userInput: event.value
+            userInput: event.target.searchbar.value,
+            submitted: true,
         })
-        console.log(this.state.userInput)
+
+        event.target.searchbar.value = '';
+
+        fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&key=${process.env.REACT_APP_API_KEY}&type=video&q=${this.state.userInput}`)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+            .catch((error) => {
+                console.log(error);
+                throw error;
+            })
     } 
 
     render() {
         return (
             <div>
-                <form>
-                    <input type='text' placeholder='type here' required></input>
-                    <button type='submit' onSubmit={this.handleSubmit}>Search</button>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='type here' name='searchbar' required />
+                    <button type='submit' value='search'>Search</button>
                 </form>
+                {/* {this.state.submitted ? <Thumbnail /> : 'No Search Results Yet! Please submit a search above :)'} */}
             </div>
         )
     }
