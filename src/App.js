23dfb0e9943da122b6f3057f67086d8c125fe6fb
caveman49 <caveman4949@gmail.com>
2021-12-09 14:55:14 -0500

    added images updated teamlist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import About from "./components/About";
+import About from "./components/About/About";
 import Home from "./components/Home";
 import Nav from "./components/Nav";
 
