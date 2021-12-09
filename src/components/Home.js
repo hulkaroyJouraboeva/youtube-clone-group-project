b4cf90071f03d9b3359593571bd984442c700d37
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-12-09 00:47:09 -0500

    correct link set up for fetch, searchbar clears onSubmit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,6 @@ import React from "react";
 import SearchBar from "./SearchBar.js";
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userInput: ''
-        }
-    }
-
     componentDidMount() {
     }
     
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: ''
+        }
+    }
+
+    handleSubmit = (event) => {
+        this.setState({
+            userInput: event.value
+        })
+        console.log(this.state.userInput)
+    } 
+
     render() {
         return (
             <div>
                 <form>
-                    <input type='text' placeholder='type here'></input>
-                    <button type='submit'>Search</button>
+                    <input type='text' placeholder='type here' required></input>
+                    <button type='submit' onSubmit={this.handleSubmit}>Search</button>
                 </form>
             </div>
         )
