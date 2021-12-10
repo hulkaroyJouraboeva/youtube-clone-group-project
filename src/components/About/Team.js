8fc82f8b9d343d07ad7fd56f9ec3eb5b6f438473
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-12-10 18:08:02 -0500

    added Erics CSS

--- src/components/About/Team.js
+++ src/components/About/Team.js
@@ -1,15 +1,23 @@
+import "./Team.css";
+
 export const Team = ({ teammate }) => {
   const { pic, name, bio, github, linkedin } = teammate;
   // let info = `${firstName} ${lastName} ${bio} ${github} ${linkedin}`;
 
   return (
-    <article className="bio">
-      <p>{pic}</p>
-      <h3>{name}</h3>
-      <h4>{bio}</h4>
-      <h4>{github}</h4>
-      <h4>{linkedin}</h4>
-    </article>
+    <div class="bio-card">
+      <div class="bio-card-inner">
+        <div class="bio-card-front">
+          <p>{pic}</p>
+        </div>
+        <div class="bio-card-back">
+          <h3>{name}</h3>
+          <h4>{bio}</h4>
+          <h4>{github}</h4>
+          <h4>{linkedin}</h4>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/About/TeamList.js
+++ src/components/About/TeamList.js
@@ -6,14 +6,18 @@ import steph from "./team_pics/stephgit.jpg";
 
 export const team = [
   {
-    pic: <img src={cave} alt="cave" />,
+    pic: (
+      <img src={cave} alt="cave" style={{ width: "300px", height: "300px" }} />
+    ),
     name: "Eric Cave",
     bio: "Software Engineer that is passionate about sports, video games, and my children...",
     github: <a href="https://github.com/caveman49">GitHub</a>,
     linkedin: <a href="linkedin.com/in/eric-caveman">LinkedIn</a>,
   },
   {
-    pic: <img src={hulk} alt="hulk" />,
+    pic: (
+      <img src={hulk} alt="hulk" style={{ width: "300px", height: "300px" }} />
+    ),
     name: "Hulkaroy Jouraboeva",
     bio: "I am on the road to becoming a full stack web developer at Pursuit, a 1-year intensive program that trains individuals in software engineering.",
     github: <a href="https://github.com/hulkaroyJouraboeva">GitHub</a>,
@@ -24,18 +28,30 @@ export const team = [
     ),
   },
   {
-    pic: <img src={ana} alt="ana" />,
+    pic: (
+      <img src={ana} alt="ana" style={{ width: "300px", height: "300px" }} />
+    ),
     name: "Triane Peart",
     bio: "Full Stack Engineer with a love of art & design. Most people see a jumbled mess; I see something fun for me to craft.",
     github: <a href="https://github.com/TrianePeart">GitHub</a>,
     linkedin: <a href="www.linkedin.com/in/trianepeart">LinkedIn</a>,
   },
   {
-    pic: <img src={steph} alt="steph" />,
+    pic: (
+      <img
+        src={steph}
+        alt="steph"
+        style={{ width: "300px", height: "300px" }}
+      />
+    ),
     name: "Stephanie Lucano",
     bio: "I am completing an intensive year long fellowship at Pursuit to build my career as a software engineer. I have a background in chemistry. In college I took a wide range of chemistry courses all taught very differently.",
     github: <a href="https://github.com/Stephanie-Lucano">GitHub</a>,
-    linkedin: <a href="https://www.linkedin.com/in/stephanie-lucano-b84764150/">LinkedIn</a>,
+    linkedin: (
+      <a href="https://www.linkedin.com/in/stephanie-lucano-b84764150/">
+        LinkedIn
+      </a>
+    ),
   },
 ];
 
