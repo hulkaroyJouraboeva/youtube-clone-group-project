dc231389c45577c02de969c49dc955c8883b18d1
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2021-12-14 10:47:11 -0500

    test

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,6 +2,7 @@ import React from 'react';
 import YouTube from 'react-youtube';
 import Comment from '../Comment';
 import { useParams } from  'react-router-dom';
+import './Home.css';
 
 const _onReady = (event) => {
     event.target.pauseVideo();
@@ -10,7 +11,7 @@ const _onReady = (event) => {
 const Video = () => {
     const { id } = useParams();
     return (
-        <div>
+        <div className='Video'>
             <YouTube videoId={id} onReady={_onReady} />
             <Comment />
         </div>
